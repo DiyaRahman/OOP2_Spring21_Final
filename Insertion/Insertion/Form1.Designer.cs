e77dba2926d4e1bb025fde9b5308e605c7bfbb6b
DiyaRahman <drahman024@gmail.com>
2021-04-12 14:02:13 +1030

    Insertion

---
+++ Insertion/Insertion/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Insertion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CodeTB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name";
+            // 
+            // NameTB
+            // 
+            this.NameTB.Location = new System.Drawing.Point(208, 38);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(255, 40);
+            this.NameTB.TabIndex = 1;
+            this.NameTB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Course CODE";
+            // 
+            // CodeTB
+            // 
+            this.CodeTB.Location = new System.Drawing.Point(208, 116);
+            this.CodeTB.Name = "CodeTB";
+            this.CodeTB.Size = new System.Drawing.Size(255, 40);
+            this.CodeTB.TabIndex = 5;
+            this.CodeTB.TextChanged += new System.EventHandler(this.CodeTB_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CodeTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NameTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CodeTB;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Insertion/Insertion/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Insertion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = NameTB.Text;
+            string code = CodeTB.Text;
+
+            string connectionString = @"Server=DESKTOP-V9FTOHK\SQLEXPRESS;Database=course;User Id=sa;Password=12345;";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            string query = string.Format("insert into course values('{0}','{1}')", name, code);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            int r = cmd.ExecuteNonQuery();
+            if (r > 0)
+            {
+                MessageBox.Show("Data inserted");
+            }
+            else
+            {
+                MessageBox.Show("Cannot inserted");
+            }
+        }
+
+        private void CodeTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
